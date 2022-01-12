cbcac2d8c76f73ba5b460701e5f514a852bcefe5
Vinicius-Holtman <viniholtman28@gmail.com>
2022-01-12 17:03:30 -0300

    Create form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 import Layout from "../components/Layout";
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 
 export default function Home() {
 
@@ -28,10 +29,11 @@ export default function Home() {
         <div className="flex justify-end">
           <Botao cor="green" className="mb-4">Novo Cliente</Botao>
         </div>
-        <Tabela clientes={clientes} 
+        {/* <Tabela clientes={clientes} 
         clienteSelecionado={clienteSelecionado}
         clienteExcluido={clienteExcluido} 
-        />
+        /> */}
+        <Formulario cliente={clientes[0]} />
       </Layout>
     </div>
   )
